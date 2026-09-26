e5ba1e7f50ee4eda3bc88675ad316eef1a456bb8
nkpiksalia <natalkakielbicka@gmail.com>
2026-09-26 05:29:34 +0200

    Base scene - camera, renderer, render loop

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import * as THREE from "three";
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdfe7dd);
+const app = document.getElementById("app");
+const camera = new THREE.PerspectiveCamera(
+  55,
+  app.clientWidth / app.clientHeight,
+  0.1,
+  1000,
+);
+camera.position.set(0, 5, 10);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(app.clientWidth, app.clientHeight);
+app.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
